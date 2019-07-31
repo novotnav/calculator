12e89bb733e1f48b54943548b564bd3d52cffd8b
Veronika Novotna <veronika.novotna@protonmail.com>
2019-07-31 20:28:07 +0200

    converting variables

--- calculator.py
+++ calculator.py
@@ -10,19 +10,46 @@ try:
 except ValueError:
     print("Please use only numbers 1 - 4 to choose the operation")
 
-nr_a = int(nr_a)
-nr_b = int(nr_b)
+try:
+    val = int(nr_a)
+    nr_a = int(nr_a)
+except ValueError:
+    print("Please insert only numbers")
+try:
+    val = int(nr_b)
+    nr_b = int(nr_b)
+except ValueError:
+    print("Please insert only numbers")
 
-add = nr_a + nr_b
-sub = nr_a - nr_b
-mul = nr_a * nr_b
-div = nr_a / nr_b
+if isinstance(nr_a, int) and isinstance(nr_b, int):
+    add = nr_a + nr_b
+    sub = nr_a - nr_b
+    mul = nr_a * nr_b
+    div = nr_a / nr_b
 
-if operation == 1:
-    print("The result is: " + str(add))
-if operation == 2:
-    print("The result is: " + str(sub))
-if operation == 3:
-    print("The result is: " + str(mul))
-if operation == 4:
-    print("The result is: " + str(div))
+    if operation == 1:
+        print("The result is: " + str(add))
+    if operation == 2:
+        print("The result is: " + str(sub))
+    if operation == 3:
+        print("The result is: " + str(mul))
+    if operation == 4:
+        print("The result is: " + str(div))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
